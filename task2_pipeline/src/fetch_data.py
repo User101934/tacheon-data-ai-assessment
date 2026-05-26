dc917551de1d0ab473a58351129c7a077a99f7db
Sakthi <152948408+User101934@users.noreply.github.com>
2026-05-26 16:29:18 +0530

    Implement fetch_crypto_data function with logging
    Add logging and fetch_crypto_data function to retrieve cryptocurrency data from CoinGecko API.

--- task2_pipeline/src/fetch_data.py
+++ task2_pipeline/src/fetch_data.py
@@ -1,1 +1,31 @@
+import requests
+import logging
 
+logging.basicConfig(level=logging.INFO)
+
+API_URL = "https://api.coingecko.com/api/v3/coins/markets"
+
+params = {
+    "vs_currency": "usd",
+    "order": "market_cap_desc",
+    "per_page": 20,
+    "page": 1
+}
+
+def fetch_crypto_data():
+    try:
+        logging.info("Fetching data from CoinGecko API...")
+
+        response = requests.get(API_URL, params=params)
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        logging.info(f"Successfully fetched {len(data)} records")
+
+        return data
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"API request failed: {e}")
+        return []
