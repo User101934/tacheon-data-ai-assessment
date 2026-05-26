664c9e68da3f29bbb6d554d4618391ce384499f9
Sakthi <152948408+User101934@users.noreply.github.com>
2026-05-26 20:01:48 +0530

    Implement main function for ETL pipeline

--- task2_pipeline/src/main.py
+++ task2_pipeline/src/main.py
@@ -1,1 +1,18 @@
 
+
+from fetch_data import fetch_crypto_data
+from transform import transform_data
+from load_bigquery import load_to_bigquery
+
+def main():
+
+    raw_data = fetch_crypto_data()
+
+    transformed_df = transform_data(raw_data)
+
+    print(transformed_df.head())
+
+    load_to_bigquery(transformed_df)
+
+if __name__ == "__main__":
+    main()
