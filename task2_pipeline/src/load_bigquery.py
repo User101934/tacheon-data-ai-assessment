a347dc562d66e2272481237dd4e64410975be19f
Sakthi <152948408+User101934@users.noreply.github.com>
2026-05-26 17:32:17 +0530

    Add function to load DataFrame to BigQuery

--- task2_pipeline/src/load_bigquery.py
+++ task2_pipeline/src/load_bigquery.py
@@ -1,1 +1,18 @@
+from google.cloud import bigquery
+import logging
 
+logging.basicConfig(level=logging.INFO)
+
+def load_to_bigquery(df):
+
+    client = bigquery.Client.from_service_account_json(
+        "project-497511-9d7d340b8d78.json"
+    )
+
+    table_id = "project-497511.crypto_pipeline.crypto_market"
+
+    job = client.load_table_from_dataframe(df, table_id)
+
+    job.result()
+
+    logging.info("Data successfully loaded into BigQuery")
