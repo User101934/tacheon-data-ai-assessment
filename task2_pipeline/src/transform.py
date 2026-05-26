1731db9c48e5f3a9f8451021bc99acc3068d6f8f
Sakthi <152948408+User101934@users.noreply.github.com>
2026-05-26 16:32:50 +0530

    Add data transformation function for cryptocurrency

--- task2_pipeline/src/transform.py
+++ task2_pipeline/src/transform.py
@@ -1,1 +1,37 @@
+import pandas as pd
 
+def transform_data(data):
+
+    df = pd.json_normalize(data)
+
+    selected_columns = [
+        "id",
+        "symbol",
+        "name",
+        "current_price",
+        "market_cap",
+        "total_volume",
+        "price_change_percentage_24h"
+    ]
+
+    df = df[selected_columns]
+
+    df.columns = [
+        "coin_id",
+        "symbol",
+        "coin_name",
+        "current_price",
+        "market_cap",
+        "total_volume",
+        "price_change_24h"
+    ]
+
+    df["market_status"] = df["price_change_24h"].apply(
+        lambda x: "Growing" if x > 0 else "Declining"
+    )
+
+    df["volume_to_marketcap_ratio"] = (
+        df["total_volume"] / df["market_cap"]
+    )
+
+    return df
